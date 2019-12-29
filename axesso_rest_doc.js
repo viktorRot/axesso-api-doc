b7fa7d1bff238e6e3b9a137257fab0eb246692a0
Viktor <viktor.rotaermel@nttdata.com>
2019-12-29 17:01:35 +0100

    Update amz lookup product doc, add doc for new endpoint wlm

--- axesso_rest_doc.js
+++ axesso_rest_doc.js
@@ -1,7 +1,7 @@
 
 /**
  * @api {get} /amz/amazon-lookup-product Request Product Information
- * @apiVersion 1.1.0
+ * @apiVersion 1.2.0
  * @apiName GetProductInformation
  * @apiGroup Amazon
  *
@@ -36,6 +36,10 @@
  * @apiSuccess {String} productDetails.name  Name of the product detail.
  * @apiSuccess {Array} productDetails.value  Value of the product detail.
  * @apiSuccess {Boolean} addon Indication for addon products.
+ * @apiSuccess {Boolean} pantry Indication for pantry products.
+ * @apiSuccess {String} minimalQuantity Minimum quantity to purchase.
+ * @apiSuccess {Number} dealPrice Price if product is a deal.
+ * @apiSuccess {Number} salePrice Price if product is on sale.
  *
  * @apiSuccessExample Success-Response-example:
  *     HTTP/1.1 200 OK
@@ -75,6 +79,10 @@
  *       }
  *	  "prime": false
  *	  "addon": false
+ *	  "pantry": false
+ *	  "minimalQuantity": null
+ *	  "dealPrice": 71.42
+ *	  "salePrice": 0.0
  *	}
  *
  *
@@ -284,7 +292,7 @@
  * @apiSuccess {String} offer.sellerRating  Rating of the seller in stars.
  * @apiSuccess {String} offer.sellerRatingPercentage  Rating of the seller in percentage.
  * @apiSuccess {Boolean} offer.prime Indication for prime products.
-
+ *
  *
  * @apiSuccessExample Success-Response-example:
  *     HTTP/1.1 200 OK
@@ -403,7 +411,136 @@
  *       }
  *	 }
  *
+ */
+ 
+ 
+ /**
+ * @api {get} /wlm/walmart-lookup-product Request Product Information
+ * @apiVersion 1.0.0
+ * @apiName GetProductInformation
+ * @apiGroup Walmart
  *
+ * @apiHeader Authorization API key required. 
+ * @apiHeader Content-Type (application/json).
+ * @apiParam {String} url URL for product page (required).
+ * @apiParamExample {query} Input-Example
+ *    http://api-wlm.axesso.de/wlm/walmart-lookup-product?url=https://www.walmart.com/ip/46271127
+ *
+ * @apiSuccess {String} responseStatus 	Response status of request.
+ * @apiSuccess {String} responseMessage  Response Message of request.
+ * @apiSuccess {String} productTitle  Title of the product.
+ * @apiSuccess {String} manufacturer  Manufacturer of the product.
+ * @apiSuccess {Int} countReview  Number of reviews.
+ * @apiSuccess {String} productRating  Rating of the product.
+ * @apiSuccess {Int} walmartItemId  Item id of the product. 
+ * @apiSuccess {Array} sizeSelection Selection of sizes, if available.
+ * @apiSuccess {String} soldAndShippedBy  Retailer selling and fulfilling the purchase.
+ * @apiSuccess {Boolean} available Indication of availability.
+ * @apiSuccess {Number} oldPrice Old price.
+ * @apiSuccess {Number} price Actual price.
+ * @apiSuccess {String} shippingInformation Information to the shipping.
+ * @apiSuccess {Array} productHighlights List of highlights of the product.
+ * @apiSuccess {Array} imageUrlList List containing the urls to the product images.
+ * @apiSuccess {Object[]} productDetails  List of product details e.g. size and best seller ranking.
+ * @apiSuccess {String} productDetails.name  Name of the product detail.
+ * @apiSuccess {Array} productDetails.value  Value of the product detail.
+ *
+ * @apiSuccessExample Success-Response-example:
+ *     HTTP/1.1 200 OK
+ *{
+ *    "responseStatus": "PRODUCT_FOUND_RESPONSE",
+ *    "responseMessage": "Product successfully found!",
+ *     "productTitle": "X Rocker X-Pro 300 Black Pedestal Gaming Chair Rocker with Built-in Speakers",
+ *    "manufacturer": "X Rocker",
+ *     "walmartItemId": 554348865,
+ *    "countReview": 277,
+ *    "productRating": "4.0 Stars",
+ *    "soldAndShippedBy": "Walmart",
+ *    "oldPrice": 129.0,
+ *    "price": 108.0,
+ *    "shippingInformation": "Delivery not available",
+ *    "productHighlights": [
+ *        "Dimensions: 27.36L x 22.44W x 19.69H in",
+ *         "Long-lasting plastic frame",
+ *         "Breathable black fabric",
+ *         "Swivel and rocker pedestal base",
+ *        "Bluetooth speakers and subwoofer"
+ *    ],
+ *    "imageUrlList": [
+ *        "https://i5.walmartimages.com/asr/e9a514cb-3a64-40ff-8ddf-2b39497160c3_1.aa839ceae3ab602481b8f536b44fb36f.jpeg"
+ *    ],
+ *    "productDetails": [
+ *        {
+ *            "name": "Brand",
+ *            "value": "X Rocker"
+ *        },
+ *        {
+ *            "name": "Age Group",
+ *            "value": "Adult Teen Child"
+ *         },
+ *         {
+ *             "name": "Features",
+ *              "value": "2 speakers for total immersion surround sound\nPowerful subwoofer\nBuilt-in Bluetooth"
+ *         },
+ *        {
+ *             "name": "Color",
+ *            "value": "Black"
+ *        },
+ *        {
+ *            "name": "Gender",
+ *            "value": "Unisex"
+ *        },
+ *        {
+ *             "name": "Model",
+ *             "value": "5150001"
+ *         },
+ *         {
+ *            "name": "Manufacturer Part Number",
+ *            "value": "5150001"
+ *        },
+ *        {
+ *             "name": "Type",
+ *            "value": "Video Game Chairs Gaming Desktops"
+ *        },
+ *        {
+ *            "name": "Assembled Product Dimensions (L x W x H)",
+ *            "value": "27.36 x 22.44 x 19.69 Inches"
+ *        }
+ *    ],
+ *    "available": false
+ * }
+ *
+ *
+ * @apiErrorExample Error-Response-400:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "logref": "error",
+ *       "message": "Required String parameter 'url' is not present",
+ *       "links": []
+ *     }
+ *
+ * @apiError MissingParameter Some required parameter are missing.
+ * @apiError ProductNotFound The product for the requested url were not found.
+ *
+ * @apiErrorExample Error-Response-404:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "logref": "error",
+ *       "message": "could not find product with url: 'https://www.amazon.com/dummy'",
+ *       "links": []
+ *     }
+ *
+ * @apiError IncorrectUrl The provided url were incorrect.
+ *
+ * @apiErrorExample Error-Response-406:
+ *     HTTP/1.1 406 Not Acceptable
+ *     {
+ *       "logref": "error",
+ *       "message": "the url parameter is incorrect: 'htts://wwe.amazon.com/dummy'",
+ *       "links": []
+ *     }
  *
  *
  */
+ 
+ 
